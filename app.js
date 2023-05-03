87a87c43fc5bc8adebb0850c87b05d1c762063c9
Kevin <kevinpihlak321@gmail.com>
2023-05-03 13:58:52 +0300

    first beta stuff lul

---
+++ app.js
@@ -0,0 +1,26 @@
+const taskList = document.getElementById('taskList');
+const taskInput = document.getElementById('task');
+const timeInput = document.getElementById('time');
+const addButton = document.getElementById('add');
+
+let tasks = [];
+
+addButton.addEventListener('click', () => {
+  const task = taskInput.value;
+  const time = timeInput.value;
+  if (task && time) {
+    tasks.push({ task, time });
+    taskInput.value = '';
+    timeInput.value = '';
+    displayTasks();
+  }
+});
+
+function displayTasks() {
+  taskList.innerHTML = '';
+  for (const task of tasks) {
+    const li = document.createElement('li');
+    li.textContent = `${task.task} - ${task.time}`;
+    taskList.appendChild(li);
+  }
+}
